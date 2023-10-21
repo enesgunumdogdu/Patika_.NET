20137f0b118579691de5e9917c1ff44770560cfd
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-10-21 23:20:54 +0300

    Constructor

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -19,9 +19,17 @@ namespace Classes
             calisan1.No = 0213;
             calisan1.Departman = "IT";
             Calisan calisan2 = new Calisan { Ad = "Emre", Soyad = "Başpınar", No = 25441, Departman = "Front-end" };
+            Calisan calisan3 = new Calisan ( "Emir", "Günümdoğdu", 83712, "Muhasebe" );
+            Calisan calisan4 = new Calisan ("Mehmet", "Kısakol");
             calisan1.CalisanBilgileri();
             Console.WriteLine("\n");
             calisan2.CalisanBilgileri();
+            Console.WriteLine("\n");
+            calisan3.CalisanBilgileri();
+            Console.WriteLine("\n");
+            calisan4.CalisanBilgileri();
+
+
 
 
 
@@ -34,6 +42,24 @@ namespace Classes
         public string Soyad;
         public int No;
         public string Departman;
+
+        //Constructor
+        public Calisan(string ad, string soyad, int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+
+        public Calisan(){}
+
 
         public void CalisanBilgileri()
         {
