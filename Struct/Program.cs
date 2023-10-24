acbec45e9b4f798fc5c2ecb03dfc6c28ad300667
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-10-24 22:22:49 +0300

    Struct

---
+++ Struct/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.kisaKenar = 3;
+            dikdortgen.uzunKenar = 4;
+            Console.WriteLine("Class alan hesabı = ->" + dikdortgen.AlanHesapla());
+
+            Dikdortgen_struct dikdortgenStruct = new Dikdortgen_struct();
+
+            //Dikdortgen_struct dikdortgenStruct = new Dikdortgen_struct(3,4);
+            //Dikdortgen_Struct dikdortgen_Struct;
+            dikdortgenStruct.kisaKenar = 5;
+            dikdortgenStruct.uzunKenar = 6;
+            Console.WriteLine("Struct alan hesaabı = -> " + dikdortgenStruct.AlanHesapla());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+
+        public Dikdortgen() {
+            kisaKenar = 4;
+            uzunKenar=8;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+
+    struct Dikdortgen_struct
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+
+        public Dikdortgen_struct()
+        {
+            kisaKenar = 5;
+            uzunKenar = 6;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+}
