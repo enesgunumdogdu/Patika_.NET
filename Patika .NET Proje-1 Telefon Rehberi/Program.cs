c73a896a54af4156d16b7f00301cac6304d3626e
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-10-27 00:03:26 +0300

    Menu design, first commit

---
+++ Patika .NET Proje-1 Telefon Rehberi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Telefon_Rehberi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz: ");
+            Console.WriteLine("(1) Yeni Numara Kaydetmek");
+            Console.WriteLine("(2) Varolan Numarayı Silmek");
+            Console.WriteLine("(3) Varolan Numarayı Güncelleme");
+            Console.WriteLine("(4) Rehberi Listelemek");
+            Console.WriteLine("(5) Rehberde Arama Yapmak");
+            Console.ReadLine();
+        }
+    }
+}
