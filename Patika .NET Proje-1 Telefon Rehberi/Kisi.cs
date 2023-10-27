0f2e903869ac433fa2597e5f11e022f4ad8da18f
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-10-27 22:59:02 +0300

    Phone Directory Console App

---
+++ Patika .NET Proje-1 Telefon Rehberi/Kisi.cs
@@ -0,0 +1,9 @@
+namespace Telefon_Rehberi
+{
+    class Kisi
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string TelefonNumarasi { get; set; }
+    }
+}
