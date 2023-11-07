78501d2bb851413d8a273d5f262191a9d723a0a9
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-11-07 16:49:49 +0300

    Draw A Triangle

---
+++ Projects/Draw a Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DrawATriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawTriangle();
+        }
+
+        static void DrawTriangle()
+        {
+            Console.Write("Lütfen çizilecek dik üçgenin yüksekliğini girin: \n\n");
+            int yukseklik = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= yukseklik; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
